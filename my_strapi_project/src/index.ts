84969882e9dbbc62dc6e4ebe41872cecac58f28f
AviralM <aviralmehndiratta@gmail.com>
2025-07-23 05:05:01 +0530

    Updated Dockerfile: v3

--- my_strapi_project/src/index.ts
+++ my_strapi_project/src/index.ts
@@ -1,20 +1,10 @@
-// import type { Core } from '@strapi/strapi';
+// src/index.ts
 
-export default {
-  /**
-   * An asynchronous register function that runs before
-   * your application is initialized.
-   *
-   * This gives you an opportunity to extend code.
-   */
-  register(/* { strapi }: { strapi: Core.Strapi } */) {},
+import "@strapi/types"; // Ensures core types are available
 
-  /**
-   * An asynchronous bootstrap function that runs before
-   * your application gets started.
-   *
-   * This gives you an opportunity to set up your data model,
-   * run jobs, or perform some special logic.
-   */
-  bootstrap(/* { strapi }: { strapi: Core.Strapi } */) {},
-};
+declare module "@strapi/strapi" {
+  interface ContentTypes {
+    "api::post.post": any;
+    "api::blogpost.blogpost": any;
+  }
+}
