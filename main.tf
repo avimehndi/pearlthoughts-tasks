9d0173ba6afa0ebec9e8ed2dd7ee6d81d410d3de
Aviral Mehndiratta <145124066+avimehndi@users.noreply.github.com>
2025-07-22 23:43:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -45,15 +45,19 @@ resource "aws_security_group" "strapi_sg" {
   }
 }
 
+data "template_file" "user_data" {
+  template = file("${path.module}/user-data.sh")
+  vars = {
+    docker_tag = var.docker_image_tag
+  }
+}
+
 resource "aws_instance" "strapi_ec2" {
   ami                         = data.aws_ami.ubuntu_jammy.id
   instance_type               = var.instance_type
   key_name                    = var.key_name
   associate_public_ip_address = true
 
-  user_data = templatefile("${path.module}/user-data.sh", {
-    docker_tag = var.docker_image_tag
-  })
 
   vpc_security_group_ids = [aws_security_group.strapi_sg.id]
 
