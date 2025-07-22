a02625ed6a09b654d9e4ea921d2b26d0e502b14b
AviralM <aviralmehndiratta@gmail.com>
2025-07-23 01:56:59 +0530

    final commit v2

--- user_data.sh
+++ user_data.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
-apt-get update -y
-apt-get install -y docker.io
-systemctl start docker
-systemctl enable docker
 
-docker run -d -p 1337:1337 avimehndi/strapi-app:${docker_tag}
+exec > /var/log/user-data.log 2>&1
+
+sudo apt-get update -y
+sudo apt-get install -y docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
+
+sudo docker pull avimehndi/strapi-app:${docker_tag}
+
+sudo docker rm -f strapi || true
+sudo docker run -d \
+  --name strapi \
+  -p 1337:1337 \
+  --restart unless-stopped \
+  -e APP_KEYS="$APP_KEYS" \
+  -e API_TOKEN_SALT="$API_TOKEN_SALT" \
+  -e ADMIN_JWT_SECRET="$ADMIN_JWT_SECRET" \
+  -e JWT_SECRET="$JWT_SECRET" \
+  avimehndi/strapi-app:${docker_tag}
+echo ">>> Done setting up Strapi"
