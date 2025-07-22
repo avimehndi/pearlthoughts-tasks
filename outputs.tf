33d808540df5663ce0f4abe76cd0a22172eaa338
AviralM <aviralmehndiratta@gmail.com>
2025-07-22 22:57:17 +0530

    fix: update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,3 @@
-variable "docker_image_tag" {
-  description = "Docker image tag to deploy"
-  type        = string
-}
-
 output "ec2_public_ip" {
   description = "Public IP of the EC2 instance"
   value       = aws_instance.strapi_server.public_ip
